a0052aca72e2eaf4505ad42671f24e48b4b1c4d7
Thiago Santana Pereira <thiago.pereira@guide.com.br>
2022-06-27 20:16:21 -0300

    url de comunicação

--- lib/http/webclients/onboarding_webclient.dart
+++ lib/http/webclients/onboarding_webclient.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:http/http.dart';
 import 'package:http_interceptor/http/intercepted_client.dart';
 import '../../models/onboarding.dart';
@@ -9,19 +8,22 @@ import '../interceptors/logging-interceptor.dart';
 final Client client =
     InterceptedClient.build(interceptors: [LoggingInterceptor()]);
 
-const String baseUrl = '10.45.1.98:5010';
+const String baseUrl = 'onboarding-acquisition.qa.guideinvestimentos.com.br';
 const String baseUrlEndpoint = 'acquisition';
 
 class OnboardingWebClient {
   Future<OnboardingResult> save(Onboarding transaction) async {
-    final String transactionJson =
-        jsonEncode(_populateFromJson(transaction.cpf));
+    final String teste = _populateFromJson(transaction.cpf);
+
+    final String transactionJson = _populateFromJson(transaction.cpf);
 
     final Response response = await client.post(
-      Uri.http(baseUrl, baseUrlEndpoint),
+      Uri.https(baseUrl, baseUrlEndpoint),
       headers: {
         'Content-type': 'application/json',
-        'password': '1000',
+        'projectHash': '4001f984-a630-4645-b810-00cc66f1b108',
+        'Authorization':
+            'Bearer eyJraWQiOiJEWDlONSs5dXJwMjBpSFRxa2ZBMURBckF5YTUzQU16Y1ByVkMwZ3A1S0RBPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiIyZzVtY2Foa3Vzc2dwYmgzZHNpZDQzMmJtaSIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiZ3VpZGVcL3ZhbGVtb2JpLnJlYWQgZ3VpZGVcL3ZhbGVtb2JpLndyaXRlIiwiYXV0aF90aW1lIjoxNjU2MzY5ODc2LCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAuc2EtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3NhLWVhc3QtMV9zd3pMRGhTYTciLCJleHAiOjE2NTYzNzM0NzYsImlhdCI6MTY1NjM2OTg3NiwidmVyc2lvbiI6MiwianRpIjoiMWRhMDI1N2UtMThkNC00ZDJkLThlMzgtNTE4ZDEyNGY5MmNjIiwiY2xpZW50X2lkIjoiMmc1bWNhaGt1c3NncGJoM2RzaWQ0MzJibWkifQ.YxzebqVTOsKEZzkg9s0f3S1TXD4vvuAki_-PxNTWtmSSnlLUUv7gZhghKH9YzTVpxxW7yov_36Jn-XEopmJaDsVIePjqfsP4S1l5Es5mVXzpk9E9vXeuU__MQTHfFOD9hhFJky6P1GIntNhC38S1APS-1U938je3Qqv18J15XdN73AbPuCdmAc0dnjGHQTigWICBhlHeNFDpcZnkD3SN_5jYPg3-DqLjcXLh66YQB2hU7Kv8ywHtxXJdSNZhvXIo2mLj4CRehl2Xfl335XkdL04yR-JQNuJpKo6LWTMbpfGJO_LHxRIV3wHWHs39HNAEbkeFG3dV-0NnbkMr6pT4uQ',
       },
       body: transactionJson,
     );
